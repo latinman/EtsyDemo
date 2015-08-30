652fe84d60b96b3f0dd5ebf18e5f204af77adeed
latinman <erivas78@gmail.com>
2015-08-30 10:46:36 -0400

    Added name field to users

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,7 +1,4 @@
 class Listing < ActiveRecord::Base
-  validates :name, presence: true
-  validates :description, presence: true
-  validates :price, numericality: { greater_than_0: true }
   if Rails.env.development?
     has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "default.png"
   else
@@ -11,4 +8,7 @@ class Listing < ActiveRecord::Base
                       :path => ":style/:id_:filename"
     validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
   end
+    validates :name, presence: true
+    validates :description, presence: true
+    validates :price, numericality: { greater_than_0: true }
 end
