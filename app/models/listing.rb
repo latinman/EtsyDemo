08ae6b4fe7172e32ebaf4fca05ba8900442eb170
latinman <erivas78@gmail.com>
2015-08-30 12:25:57 -0400

    Fixed image error in localhost

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,13 +1,6 @@
 class Listing < ActiveRecord::Base
-  if Rails.env.development?
-    has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "default.png"
-  else
-    has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "default.png",
-                      :storage => :dropbox,
-                      :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-                      :path => ":style/:id_:filename"
-    validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
-  end
+    has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "default.png"
+    validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
     validates :name, presence: true
     validates :description, presence: true
     validates :price, numericality: { greater_than: 0 }
