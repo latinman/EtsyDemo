0519c6127ca901570e42028223b76260ca8a9c30
latinman <erivas78@gmail.com>
2015-08-30 22:21:34 -0400

    Added order history pages

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,18 +1,18 @@
 class OrdersController < ApplicationController
-  before_action :set_order, except: [:index, :new, :create]
+  before_action :set_order, except: [:sales, :purchases, :index, :new, :create]
   before_action :authenticate_user!
 
-  def index
-    @orders = Order.all
+  def sales
+    @orders = Order.all.where(seller: current_user).order("created_at DESC")
+  end
+
+  def purchases
+    @orders = Order.all.where(buyer: current_user).order("created_at DESC")
   end
 
   def new
     @order = Order.new
     @listing = Listing.find(params[:listing_id])
-  end
-
-  def show
-
   end
 
   def create
@@ -31,25 +31,6 @@ class OrdersController < ApplicationController
       flash[:danger] = "Couldn't create order."
       render "new"
     end
-  end
-
-  def edit
-
-  end
-
-  def update
-
-    if @order.update(order_params)
-      flash[:success] = "Order updated!"
-      redirect_to orders_path
-    else
-      render "edit"
-    end
-  end
-
-  def destroy
-    @order.destroy
-    redirect_to orders_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :listings do
-    resources :orders
+    resources :orders, only: [:new, :create]
   end
   get "/seller" => "listings#seller"
   get "/about" => "pages#about"
   get "/contact" => "pages#contact"
+  get "/sales" => "orders#sales"
+  get "/purchases" => "orders#purchases"
   root "listings#index"
 end
